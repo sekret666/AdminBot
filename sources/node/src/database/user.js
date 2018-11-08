64ce367d3ff11ef6bc58662beaef06896c347adf
Kamal (Riwogo) <pmer93@gmail.com>
2018-11-08 20:26:53 +0330

    add methods to User model

--- sources/node/src/database/user.js
+++ sources/node/src/database/user.js
@@ -1,10 +1,32 @@
 var exports = module.exports = {};
 
 exports.createModel = (sequelize, DataTypes) => {
-    return sequelize.define('user', {
+    const User = sequelize.define('user', {
         tgId: {type: DataTypes.STRING, unique: true},
         name: DataTypes.STRING,
         lastname: DataTypes.STRING,
         warnsNumber: DataTypes.INTEGER.UNSIGNED
     })
+
+    User.findByTgId = async function (userTgId) {
+        const user = await this.findOne({
+            where: {
+                tgId: userTgId
+            } 
+        })
+
+        return user
+    }
+
+    User.updateWarnsNumberByTgId = async function(newWarnsNum, userTgId) {
+        await User.update({
+            warnsNumber: newWarnsNum,
+          }, {
+                where: {
+                    tgId: userTgId
+            }
+        });
+    }
+
+    return User
 }
