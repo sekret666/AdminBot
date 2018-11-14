f7d2f2b9ad95792679c6554cbdc64d8b6ccda682
Kamal (Riwogo) <pmer93@gmail.com>
2018-11-14 14:05:00 +0330

    add spam methods to Database class

--- sources/node/src/database/db_manager.js
+++ sources/node/src/database/db_manager.js
@@ -24,52 +24,130 @@ const Group = GroupModel.createModel(sequelize, Sequelize)
 const Spam = SpamModel.createModel(sequelize, Sequelize)
 const ClearPeriod = ClearPeriodModel.createModel(sequelize, Sequelize)
 
-const dbManager = setDatabaseScheme()
-dbManager.then(() => {
-    doWorks()
-})
+let _created = false
 
+class Database {
+
+    static get created() {
+        return _created 
+    }
+
+    async init() {
+        if (Database.created) {
+            return
+        }
+        
+        const UserGroup = sequelize.define('UserGroup', {
+            isAdmin: {type: Sequelize.BOOLEAN, defaultValue: false}
+        })
+        
+        User.belongsToMany(Group, {through: UserGroup})
+        Group.belongsToMany(User, {through: UserGroup})
+    
+        User.hasMany(User, {as: 'Childs'})
+        User.belongsTo(User)
+    
+        Spam.belongsToMany(Group, {through: "SpamGroup"})
+        Group.belongsToMany(Spam, {through: "SpamGroup"})
+    
+        Group.hasMany(ClearPeriod)
+        
+        await sequelize.sync({force: true})
+        _created = true
+    }
+
+    async is_spam(groupTgId, text) {
+        const group = await Group.findByTgId(groupTgId)
+        const spam = await Spam.findByText(text)
+
+        return (await group.hasSpam(spam))
+    }
+
+    async has_spam(groupTgId, texts) {
+        for (let i = 0; i < texts.length; i++) {
+            const check = await this.is_spam(groupTgId, texts[i]);
+            
+            if (check) {
+                return true
+            }
+        }
+
+        return false
+    }
+    
+    async add_spam(groupTgId, text) {
+        const group = await Group.findByTgId(groupTgId)
+        const [spam,_] = await Spam.findOrCreate({
+            where: {
+                text: text
+            }
+        })
+
+        await group.addSpam(spam)
+    }
+    
+    async remove_spam(groupTgId, text) {
+        const group = await Group.findByTgId(groupTgId)
+        const spam = await Spam.findByText(text)
+
+        await group.removeSpam(spam)
+    }
+    
+    async get_spams(groupTgId) {
+        const group = await Group.findByTgId(groupTgId)
+        
+        return (await group.getSpams())
+    }
+    
+    async set_spams(groupTgId, texts) {
+        const group = await Group.findByTgId(groupTgId)
+
+        const spams = []
+        for (let i = 0; i < texts.length; i++) {
+            const [spam,_] = await Spam.findOrCreate({
+                where: {
+                    text: texts[i]
+                }
+            })
+            
+            spams.push(spam)
+        }
+
+        await group.setSpams(spams)
+    }
+}
+
+doWorks()
 
 async function doWorks() {
+    const dbManager = new Database()
+    await dbManager.init()
+     
     const gp = await Group.createByNameAndTgId('azhant', 'azz')
-    const clearP = await ClearPeriod.create({from:1, to:8}) 
-    await gp.addClearPeriod(clearP)
-    const ps = await gp.hasClearPeriod(clearP)
-    console.log(ps);
-}
-
-function setDatabaseScheme() {
-    const UserGroup = sequelize.define('UserGroup', {
-        isAdmin: {type: Sequelize.BOOLEAN, defaultValue: false}
-    })
+    const sp = await Spam.create({text: 'fuck'})
+    const sp2 = await Spam.create({text: 'fuck2'})
     
-    User.belongsToMany(Group, {through: UserGroup})
-    Group.belongsToMany(User, {through: UserGroup})
+    await dbManager.add_spam(gp.tgId, sp.text)
+    // await dbManager.add_spam(gp.tgId, sp2.text)
+    await dbManager.set_spams(gp.tgId, [sp2.text, 'kir'])
 
-    User.hasMany(User, {as: 'Childs'})
-    User.belongsTo(User)
-
-    Spam.belongsToMany(Group, {through: "SpamGroup"})
-    Group.belongsToMany(Spam, {through: "SpamGroup"})
-
-    Group.hasMany(ClearPeriod)
-
-    return sequelize.sync({force: true})
+    const check = await dbManager.has_spam(gp.tgId, [sp.text, sp2.text])
+    console.log(check);
+    
 }
-
 
 
 function getDatabaseConfig() {
     return JSON.parse(fs.readFileSync(dbConfigFilePath));
 }
 
-// function getMethods(obj)
-// {
-//     var res = [];
-//     for(var m in obj) {
-//         if(typeof obj[m] == "function") {
-//             res.push(m)
-//         }
-//     }
-//     return res;
-// }
+function getMethods(obj)
+{
+    var res = [];
+    for(var m in obj) {
+        if(typeof obj[m] == "function") {
+            res.push(m)
+        }
+    }
+    return res;
+}
--- sources/node/src/database/spam.js
+++ sources/node/src/database/spam.js
@@ -1,7 +1,19 @@
 var exports = module.exports = {};
 
 exports.createModel = (sequelize, DataTypes) => {
-    return sequelize.define('spam', {
-        text: DataTypes.STRING,
+    const Spam = sequelize.define('spam', {
+        text: {type: DataTypes.STRING, unique: true}
     })
+
+    Spam.findByText = async function(text) {
+        const spam = await this.findOne({
+            where: {
+                text: text
+            } 
+        })
+
+        return spam
+    }
+
+    return Spam
 }
